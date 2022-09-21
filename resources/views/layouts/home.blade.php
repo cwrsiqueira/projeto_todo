a42605393539592492a8a30d18ef5e36500a1246
cwrsiqueira <cwrsiqueira@msn.com>
2022-09-21 04:44:18 -0300

    Layout dashboard

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,7 +13,7 @@
 <body>
     <div class="container">
         <div class="sidebar">
-            Logo
+            <img src="{{ asset('storage/assets/images/logo.png') }}" alt="logo">
         </div>
         <div class="content">
             <nav>
@@ -26,18 +26,60 @@
                             <h2>Progresso do dia</h2>
                             <div class="line"></div>
                             <div class="date-area">
-                                <div class="date-area-btn left"> &lt; </div>
+                                <div class="date-area-btn left">
+                                    <img src="{{ asset('storage/assets/images/icon-prev.png') }}" alt="prev">
+                                </div>
                                 <div class="date-area-center"> 13 de Dez </div>
-                                <div class="date-area-btn righ"> &gt; </div>
+                                <div class="date-area-btn righ">
+                                    <img src="{{ asset('storage/assets/images/icon-next.png') }}" alt="next">
+                                </div>
                             </div>
                         </div>
                         <div class="graph-info-area-bot">
                             Tarefas: <b>3/6</b>
                         </div>
                     </div>
+                    <div class="graph-img-area">
+                        <img src="{{ asset('storage/assets/images/graph.png') }}" alt="graph">
+                    </div>
+                    <div class="graph-msg-area">
+                        <img src="{{ asset('storage/assets/images/icon-info.png') }}" alt="info">
+                        Restam 3 tarefas a serem realizadas
+                    </div>
                 </section>
                 <section class="list">
-                    Lista
+                    <div class="select_tasks">
+                        <select name="" id="">
+                            <option value="">Todas as tarefas</option>
+                        </select>
+                    </div>
+                    <div class="area_tasks">
+                        @for ($i = 0; $i < 6; $i++)
+                            <div class="area_tasks_item">
+                                <div class="area_tasks_item_title">
+
+                                    <div id="inputPreview">
+                                        <input name="cssCheckbox" id="demo_opt_{{ $i }}" type="checkbox"
+                                            class="css-checkbox">
+                                        <label for="demo_opt_{{ $i }}">
+                                            <div class="text">Fazer tal coisa pra fulano</div>
+                                        </label>
+                                    </div>
+
+                                </div>
+                                <div class="area_tasks_item_priority">
+                                    <div class="priority_mark"></div>
+                                    <div class="priority_description">Alta Prioridade</div>
+                                </div>
+                                <div class="area_tasks_item_actions">
+                                    <img src="{{ asset('storage/assets/images/icon-edit.png') }}" alt="edit"
+                                        title="Editar Tarefa" class="btn-edit">
+                                    <img src="{{ asset('storage/assets/images/icon-delete.png') }}" alt="delete"
+                                        title="Excluir Tarefa" class="btn-delete">
+                                </div>
+                            </div>
+                        @endfor
+                    </div>
                 </section>
             </main>
         </div>
